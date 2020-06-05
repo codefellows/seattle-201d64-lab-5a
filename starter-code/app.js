649cb08a74fe6448fdc34571963ee809a32c0ef4
Amelia Waggoner <wildginger@wavecable.com>
2020-06-05 13:35:39 -0700

    completed question 1

--- starter-code/app.js
+++ starter-code/app.js
@@ -9,11 +9,17 @@ Test this function by hand in the console to get it working, and when you think 
 
 // Write your code here
 function sum(a, b) { //eslint-disable-line
+console.log(a,b);
+var addedNum = a + b;
+var concatenatedString = 'The sum of '+ a + ' and ' + b + ' is ' + addedNum + '.';
+var myOutput = [addedNum, concatenatedString];
 
+console.log('myOutput', myOutput);
+console.log('The sum of 4 and 7 is 11.');
+return myOutput;
 }
-
 // Here is the test for sum(); uncomment it to run it
-// testSum(4, 7);
+testSum(4, 7);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for your work on the next question!
 
