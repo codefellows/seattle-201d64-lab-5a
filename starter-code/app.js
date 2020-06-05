9efd0d7f1ddc402bdf12b40b3b4d2ce10c675d11
Tia Low <lowtia@gmail.com>
2020-06-05 15:53:39 -0700

    Question two passed yeayuh

--- starter-code/app.js
+++ starter-code/app.js
@@ -14,7 +14,7 @@ Test this function by hand in the console to get it working, and when you think 
 
 function sum(num1, num2) { //eslint-disable-line
   var getSum = num1+num2;
-  var stringWords = 'The sum of ' + num1 + ' and ' + num2 + ' is 11.';
+  var stringWords = 'The sum of ' + num1 + ' and ' + num2 + ' is ' + getSum + '.';
   var sumArray = [getSum, stringWords];
   console.log(sumArray);
   return sumArray;
@@ -35,12 +35,16 @@ Write a function called multiply() that takes in two numbers as arguments and re
 Test this function by hand in the console to get it working, and when you think it is finished, uncomment the call for the testMultiply() function and see if the test passes.*/
 
 // Write your code here
-function multiply(a, b) { //eslint-disable-line
-
+function multiply(num1, num2) { //eslint-disable-line
+  var getProduct = num1 * num2;
+  var stringWords = 'The product of ' + num1 + ' and ' + num2 + ' is ' + getProduct + '.';
+  console.log(stringWords);
+  var sumArray = [getProduct, stringWords];
+  return sumArray;
 }
 
 // Here is the test for multiply(); uncomment it to run it
-// testMultiply(5,9);
+testMultiply(5,9);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for your work on the next question!
 
