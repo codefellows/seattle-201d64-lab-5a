41a24a8c39babdc04e2650b31c28f6dacba2657c
richminlee <lee.richardm@gmail.com>
2020-06-05 14:51:37 -0700

    multiplyArray function in multiplyArray branch

--- starter-code/app.js
+++ starter-code/app.js
@@ -98,7 +98,7 @@ function sumArray(sumArr) { //eslint-disable-line
 
 // Here is the test for sumArray(); uncomment it to run it
 
-testSumArray(testArray);
+// testSumArray(testArray);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for your work on the next question!
 
@@ -114,11 +114,24 @@ Test this function by hand in the console to get it working, and when you think 
 
 // Write your code here
 function multiplyArray(multArr) { //eslint-disable-line
-
+  let prodOfArray = 1;
+  let val = "";
+  for(let i in multArr){
+    if(i < multArr.length-1){
+      val += multArr[i] + ",";
+    }else{
+      val += multArr[i];
+    }
+    prodOfArray *= multArr[i];
+  }
+  let words = "The numbers " + val + " have a product of " + prodOfArray + ".";
+  let result = [prodOfArray, words]
+  console.log(result);
+  return result;
 }
 
 // Here is the test for multiplyArray(); uncomment it to run it
-// testMultiplyArray(testArray);
+testMultiplyArray(testArray);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. 
 
