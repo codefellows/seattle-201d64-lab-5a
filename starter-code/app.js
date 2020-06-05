1ea9c4fd0fa0c9193973b43fe147ce62b244c7f5
Amelia Waggoner <wildginger@wavecable.com>
2020-06-05 15:32:04 -0700

    question 3 finished

--- starter-code/app.js
+++ starter-code/app.js
@@ -62,11 +62,26 @@ Test this function by hand in the console to get it working, and when you think 
 
 // Write your code here
 function sumAndMultiply(a, b, c) { //eslint-disable-line
+console.log(a,b,c);
+ var addFirstTwo = sum(a,b);
+ var finalSum = sum(addFirstTwo[0], c);  
+ 
+ var sumString = a + ' and ' + b  + ' and ' + c + ' sum to ' + finalSum[0] + '.';
 
+var multiThirdFourth = multiply(a,b);
+var finalProduct = multiply(multiThirdFourth[0], c);
+
+var multiString = 'The product of ' + a +  ' and ' + b + ' and ' + c + ' is ' + finalProduct[0] + '.';
+
+var finalOutput = [finalSum[0], finalProduct[0], sumString, multiString];
+
+console.log('4 and 7 and 5 sum to 16.');
+console.log(finalOutput, 'The product of 4 and 7 and 5 is 140.');
+return finalOutput;
 }
 
 // Here is the test for sumAndMultiply(); uncomment it to run it
-// testSumAndMultiply(4,7,5);
+testSumAndMultiply(4,7,5);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for your work on the next question!
 
