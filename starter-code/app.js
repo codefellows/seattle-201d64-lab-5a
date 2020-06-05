f00d96d8d038eb9f1e88f6a2d6698d825263d496
Court Hans <cornta@yahoo.com>
2020-06-05 14:12:39 -0700

    passed test 2

--- starter-code/app.js
+++ starter-code/app.js
@@ -8,17 +8,17 @@ Write a function called sum() that takes in two numbers as arguments and then re
 Test this function by hand in the console to get it working, and when you think it is finished, uncomment the call for the testSum() function below and check the console to see if the test passes.*/
 
 // Write your code here
-function sum(a, b) { //eslint-disable-line
-console.log(a, b);
-var addNum = a + b;
-var sentence = 'The sum of ' + a + ' and ' + b + ' is ' + addNum + '.';
-var output = [addNum, sentence];
-console.log('output', output)
-return output
-}
+// function sum(a, b) { //eslint-disable-line
+// console.log(a, b);
+// var addNum = a + b;
+// var sentence = 'The sum of ' + a + ' and ' + b + ' is ' + addNum + '.';
+// var output = [addNum, sentence];
+// console.log('output', output);
+// return output;
+// }
 
 // Here is the test for sum(); uncomment it to run it
-testSum(4, 7);
+// testSum(4, 7);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for your work on the next question!
 
@@ -32,11 +32,16 @@ Test this function by hand in the console to get it working, and when you think 
 
 // Write your code here
 function multiply(a, b) { //eslint-disable-line
-
+console.log(a,b);
+var multiplyNums = a * b;
+var words = 'The product of ' + a + ' and ' + b + ' is ' + multiplyNums + '.';
+var outputProduct = [multiplyNums, words];
+console.log('outputProduct', outputProduct);
+return outputProduct; 
 }
 
 // Here is the test for multiply(); uncomment it to run it
-// testMultiply(5,9);
+testMultiply(5,9);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for your work on the next question!
 
