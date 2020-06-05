b8294ce56fe2be304b841b888cd673b7bb44a4ae
claudiobailon <c.bailonschubert@gmail.com>
2020-06-05 14:58:59 -0700

    Solved question 1

--- starter-code/app.js
+++ starter-code/app.js
@@ -8,12 +8,20 @@ Write a function called sum() that takes in two numbers as arguments and then re
 Test this function by hand in the console to get it working, and when you think it is finished, uncomment the call for the testSum() function below and check the console to see if the test passes.*/
 
 // Write your code here
-function sum(a, b) { //eslint-disable-line
 
+function sum(num1, num2) { //eslint-disable-line
+  console.log(num1,num2);
+  var addedNumber= num1 + num2;
+  var concatenatedString= 'The sum of ' + num1 + ' and ' + num2 + ' is ' + addedNumber + '.';
+  var myOutput = [addedNumber, concatenatedString];
+  
+  console.log('myOutput', myOutput);
+  console.log('The sum of 4 and 7 is 11.');
+  return myOutput;
 }
 
 // Here is the test for sum(); uncomment it to run it
-// testSum(4, 7);
+testSum(4, 7);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for your work on the next question!
 
