5746b3a79664f11aa510ec67237d613b9b233813
Amelia Waggoner <wildginger@wavecable.com>
2020-06-05 14:05:21 -0700

    question 2 is finished

--- starter-code/app.js
+++ starter-code/app.js
@@ -33,11 +33,19 @@ Test this function by hand in the console to get it working, and when you think 
 
 // Write your code here
 function multiply(a, b) { //eslint-disable-line
+console.log(a,b);
+var multipliedNum = a * b;
+var multipliedString = 'The product of ' + a + ' and ' + b + ' is ' + multipliedNum + '.';
 
+var secondOutput = [multipliedNum, multipliedString];
+
+console.log('secondOutput', secondOutput);
+console.log('The product of 5 and 9 is 45.');
+return secondOutput;
 }
 
 // Here is the test for multiply(); uncomment it to run it
-// testMultiply(5,9);
+testMultiply(5,9);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for your work on the next question!
 
