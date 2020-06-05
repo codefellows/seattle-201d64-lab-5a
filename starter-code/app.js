cb84d3632b32d4400bf4d7a032b0f6601da05ce9
Paul OBrien <pvovideo@outlook.com>
2020-06-05 15:42:00 -0700

    app pass multiply check

--- starter-code/app.js
+++ starter-code/app.js
@@ -40,16 +40,17 @@ testSum(4, 7);
     - 1. TODO: the first element is the product of those numbers
     - 1. TODO: second element is a string that EXACTLY follows this example and uses the values that were input into the function:
           "The product of 5 and 9 is 45."
-
 Test this function by hand in the console to get it working, and when you think it is finished, uncomment the call for the testMultiply() function and see if the test passes.*/
 
 // Write your code here
 function multiply(a, b) { //eslint-disable-line
-
+  var doTheMultiply = a * b;
+  var stringMultiply = 'The product of ' + a + ' and ' + b + ' is 45.';
+  return [doTheMultiply, stringMultiply];
 }
 
 // Here is the test for multiply(); uncomment it to run it
-// testMultiply(5,9);
+testMultiply(5,9);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for your work on the next question!
 
