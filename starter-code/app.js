01887353d1f64d4ab643bf19606ad25656b0c981
Tia Low <lowtia@gmail.com>
2020-06-05 19:29:23 -0700

    Finished question five yeahhhh

--- starter-code/app.js
+++ starter-code/app.js
@@ -85,12 +85,6 @@ testSumAndMultiply(4,7,5);
 
 
 
-
-
-
-
-
-
 /* Problem 4
 Write a function called sumArray() that takes in an array of numbers as its single argument and then returns an array where the first element is the sum of the numbers in the array, and the second element is a string that EXACTLY follows this example and uses the values that were input into the function:
 
@@ -130,14 +124,6 @@ testSumArray(testArray);
 
 
 
-
-
-
-
-
-
-
-
 /* Problem 5
 Write a function called multiplyArray() that takes an array of numbers as its argument and returns an array whose first element is the product of those numbers, and the second element is a string that EXACTLY follows this example and uses the values that were input into the function:
 
@@ -150,16 +136,32 @@ Test this function by hand in the console to get it working, and when you think 
 // Write your code here
 function multiplyArray(multArr) { //eslint-disable-line
 
+  var multiplyFirstTwoNumbers = multiply(multArr[0], multArr[1])[0];
+  // console.log(multiplyFirstTwoNumbers);
+  var multiplyAllThreeNumbers = multiply(multiplyFirstTwoNumbers, multArr[2])[0];
+  // console.log(multiplyAllThreeNumbers);
+  //now i know that var multiplyAllThreeNumbers is equal to 24
+
+
+  var secondElementStringWithNumbers = 'The numbers ' + multArr[0] + ',' + multArr[1] + ',' + multArr[2] + ' have a product of ' + multiplyAllThreeNumbers + '.';
+  var questionFiveArray = [multiplyAllThreeNumbers, secondElementStringWithNumbers];
+  return questionFiveArray;
 }
 
 // Here is the test for multiplyArray(); uncomment it to run it
-// testMultiplyArray(testArray);
+testMultiplyArray(testArray);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. 
 
 // You're done! Submit the link to the repo following the instructions in Canvas. Or, try out the stretch goal below...
 
 // Don't forget to create a new branch for your work on the next question, if you attempt it.
+
+
+
+
+
+
 
 /////////////////////////////////////
 /* STRETCH GOAL: Problem 6
