48bb337bb83dd08620de0a3457d5c9a2b8cf32a0
Court Hans <cornta@yahoo.com>
2020-06-05 21:04:31 -0700

    passed test 4

--- starter-code/app.js
+++ starter-code/app.js
@@ -71,7 +71,7 @@ return sumAndMultiplyOutput;
 }
 
 // Here is the test for sumAndMultiply(); uncomment it to run it
-testSumAndMultiply(4,7,5);
+// testSumAndMultiply(4,7,5);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for your work on the next question!
 
@@ -89,12 +89,17 @@ Test this function by hand in the console to get it working, and when you think 
 var testArray = [2, 3, 4]; //eslint-disable-line
 
 function sumArray(sumArr) { //eslint-disable-line
-
+var sumFirstTwo = sum(testArray[0], testArray[1]);
+var sumTotal = sum(sumFirstTwo[0], testArray[2]);
+var sumArrayWords = testArray[0] + ',' + testArray[1] + ',' + testArray[2] + ' was passed in as an array of numbers, and ' + sumTotal[0] + ' is their sum.';
+var sumArrayOutput = [sumTotal[0], sumArrayWords];
+console.log('sumArrayOutput:', sumArrayOutput);
+return sumArrayOutput;
 }
 
 // Here is the test for sumArray(); uncomment it to run it
 
-// testSumArray(testArray);
+testSumArray(testArray);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for your work on the next question!
 
