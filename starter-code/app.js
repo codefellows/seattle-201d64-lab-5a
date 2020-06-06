358287ca1ccc20ed77da9251ef2dc6e42cf629fc
bromero <blakeromero001@gmail.com>
2020-06-05 17:16:28 -0700

    sumFunction

--- starter-code/app.js
+++ starter-code/app.js
@@ -8,12 +8,21 @@ Write a function called sum() that takes in two numbers as arguments and then re
 Test this function by hand in the console to get it working, and when you think it is finished, uncomment the call for the testSum() function below and check the console to see if the test passes.*/
 
 // Write your code here
-function sum(a, b) { //eslint-disable-line
+function sum(numOne, numTwo) { //eslint-disable-line ==returns [sum, string]
+  var integerOne = numOne;
+  console.log(integerOne);
+  var integerTwo = numTwo;
+  console.log(integerTwo);
+  var sumNumber = numOne + numTwo;
+  console.log(sumNumber);
 
+  var numString = "The sum of " + integerOne + " and " + integerTwo + " is " + sumNumber + "."
+  console.log(numString);
+  return [sumNumber, numString]
 }
 
 // Here is the test for sum(); uncomment it to run it
-// testSum(4, 7);
+testSum(4, 7);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for your work on the next question!
 
